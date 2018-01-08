908f6c9b4d1152629c45cf603681857c64e98b90
petitviolet <violethero0820@gmail.com>
2018-01-09 08:23:19 +0900

    added just logging middleware

--- modules/main/src/main/scala/net/petitviolet/prac/graphql/GraphQLServer.scala
+++ modules/main/src/main/scala/net/petitviolet/prac/graphql/GraphQLServer.scala
@@ -1,18 +1,24 @@
 package net.petitviolet.prac.graphql
 
 import akka.http.scaladsl.model.{ StatusCode, StatusCodes }
+import net.petitviolet.prac.graphql.dao.container
 import net.petitviolet.prac.graphql.scheme.SchemaDefinition
 import sangria.ast.Document
 import sangria.execution.deferred.DeferredResolver
-import sangria.execution.{ ErrorWithResolver, Executor, QueryAnalysisError }
+import sangria.execution._
 import sangria.marshalling.sprayJson._
 import sangria.parser.QueryParser
+import sangria.renderer.SchemaRenderer
 import sangria.schema.Schema
 import spray.json.{ JsObject, JsString, JsValue }
 
 import scala.concurrent.{ ExecutionContext, Future }
 
 object GraphQLServer {
+  private def schema = SchemaDefinition.schema
+
+  def showSchema: String = SchemaRenderer.renderSchema(schema)
+
   def execute(jsValue: JsValue)(implicit ec: ExecutionContext): Future[(StatusCode, JsValue)] = {
     val JsObject(fields) = jsValue
     val operation = fields.get("operationName") collect {
@@ -29,12 +35,13 @@ object GraphQLServer {
     Future.fromTry(QueryParser.parse(document)) flatMap { queryDocument =>
       // query parsed successfully, time to execute it!
       executeGraphQL(
-        SchemaDefinition.schema,
+        schema,
         queryDocument,
         vars,
         operation,
         dao.container,
-        SchemaDefinition.resolver
+        SchemaDefinition.resolver,
+        Middlewares.logging :: Nil,
       )
     }
   }
@@ -46,6 +53,7 @@ object GraphQLServer {
       operation: Option[String],
       repository: Repository,
       deferredResolver: DeferredResolver[Repository],
+      middlewares: List[Middleware[Repository]] = Nil,
   )(implicit ec: ExecutionContext): Future[(StatusCode, JsValue)] = {
     import StatusCodes._
     Executor
@@ -56,6 +64,7 @@ object GraphQLServer {
         variables = vars,
         operationName = operation,
         deferredResolver = deferredResolver,
+        middleware = middlewares,
       )
       .map { jsValue =>
         OK -> jsValue
@@ -67,4 +76,23 @@ object GraphQLServer {
 
   }
 
+}
+
+object Middlewares {
+  private val logger = new {
+    def info(s: => String): Unit = println(s"[logging]$s")
+  }
+
+  val logging: Middleware[dao.container] = new Middleware[dao.container] {
+    override type QueryVal = Unit
+
+    override def beforeQuery(context: MiddlewareQueryContext[container, _, _]): QueryVal = {
+      logger.info(s"before OperationName: ${context.operationName}")
+    }
+
+    override def afterQuery(queryVal: QueryVal,
+                            context: MiddlewareQueryContext[container, _, _]): Unit = {
+      logger.info(s"after queryVal: $queryVal")
+    }
+  }
 }
