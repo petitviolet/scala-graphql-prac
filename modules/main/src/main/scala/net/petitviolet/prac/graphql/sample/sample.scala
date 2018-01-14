3732cac7fef8669dffeeeaf8c0eeea440467d41f
petitviolet <violethero0820@gmail.com>
2018-01-14 17:13:23 +0900

    small sample

---
+++ modules/main/src/main/scala/net/petitviolet/prac/graphql/sample/sample.scala
@@ -0,0 +1,133 @@
+package net.petitviolet.prac.graphql.sample
+
+import java.util.concurrent.Executors
+
+import akka.actor.ActorSystem
+import akka.event.Logging
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import akka.http.scaladsl.model.StatusCode
+import akka.http.scaladsl.model.StatusCodes._
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server._
+import akka.stream.ActorMaterializer
+import sangria.execution._
+import sangria.macros.derive
+import sangria.marshalling.sprayJson._
+import sangria.parser.QueryParser
+import sangria.schema._
+import spray.json.{ JsObject, JsString, JsValue }
+
+import scala.concurrent.duration._
+import scala.concurrent._
+import scala.io.StdIn
+
+object sample extends App {
+  implicit val system: ActorSystem = ActorSystem("graphql-prac")
+  implicit val materializer: ActorMaterializer = ActorMaterializer()
+
+  private val executionContext =
+    ExecutionContext.fromExecutor(Executors.newFixedThreadPool(sys.runtime.availableProcessors()))
+
+  val route: Route =
+    (post & path("graphql")) {
+      entity(as[JsValue]) { jsObject =>
+        logRequestResult("/graphql", Logging.InfoLevel) {
+          complete(GraphQLServer.execute(jsObject)(executionContext))
+        }
+      }
+    } ~
+      get {
+        logRequestResult("/graphiql.html", Logging.InfoLevel) {
+          getFromResource("graphiql.html")
+        }
+      }
+
+  val host = sys.props.get("http.host") getOrElse "0.0.0.0"
+  val port = sys.props.get("http.port").fold(8080)(_.toInt)
+
+  val f = Http().bindAndHandle(route, host, port)
+
+  println(s"server at [$host:$port]")
+
+  val _ = StdIn.readLine("\ninput something\n")
+
+  println("\nshutdown...\n")
+  val x = f.flatMap { b =>
+    b.unbind()
+      .flatMap { _ =>
+        materializer.shutdown()
+        system.terminate()
+      }(ExecutionContext.global)
+  }(ExecutionContext.global)
+
+  Await.ready(x, 5.seconds)
+  sys.runtime.gc()
+  println(s"shutdown completed!\n")
+}
+
+private object GraphQLServer {
+  def execute(jsValue: JsValue)(implicit ec: ExecutionContext): Future[(StatusCode, JsValue)] = {
+    val JsObject(fields) = jsValue
+    val operation = fields.get("operationName") collect {
+      case JsString(op) => op
+    }
+
+    val vars = fields.get("variables") match {
+      case Some(obj: JsObject) => obj
+      case _                   => JsObject.empty
+    }
+
+    val Some(JsString(document)) = fields.get("query") orElse fields.get("mutation")
+
+    Future.fromTry(QueryParser.parse(document)) flatMap { queryDocument =>
+      Executor
+        .execute(
+          SchemaSample.schema,
+          queryDocument,
+          new SchemaSample.MyObjectRepository,
+          operationName = operation,
+          variables = vars
+        )
+        .map { jsValue =>
+          OK -> jsValue
+        }
+        .recover {
+          case error: QueryAnalysisError => BadRequest -> error.resolveError
+          case error: ErrorWithResolver  => InternalServerError -> error.resolveError
+        }
+    }
+  }
+}
+
+private object SchemaSample {
+  case class MyObject(id: Long, name: String)
+  class MyObjectRepository() {
+    private var data: Seq[MyObject] = MyObject(1, "alice") :: MyObject(2, "bob") :: MyObject(
+      3,
+      "charles") :: Nil
+    def findAll: Seq[MyObject] = data
+    def findById(id: Long): Option[MyObject] = data.find { _.id == id }
+  }
+
+  val myObj = derive.deriveObjectType[Unit, MyObject]()
+
+  lazy val myQuery: ObjectType[MyObjectRepository, Unit] = {
+    ObjectType.apply(
+      "MyQuery",
+      fields[MyObjectRepository, Unit](
+        {
+          val idArg = Argument("id", LongType)
+          Field(
+            "find_by_id",
+            OptionType(myObj),
+            arguments = idArg :: Nil,
+            resolve = ctx => ctx.ctx.findById(ctx.arg(idArg))
+          )
+        },
+        Field("all", ListType(myObj), resolve = ctx => ctx.ctx.findAll),
+      )
+    )
+  }
+  lazy val schema = Schema(myQuery)
+}
