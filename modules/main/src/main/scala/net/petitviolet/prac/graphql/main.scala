18eb354126b2563c6162aaf30a861393106d2b8f
petitviolet <violethero0820@gmail.com>
2018-02-17 20:07:20 +0900

    logging

--- modules/main/src/main/scala/net/petitviolet/prac/graphql/main.scala
+++ modules/main/src/main/scala/net/petitviolet/prac/graphql/main.scala
@@ -2,32 +2,51 @@ package net.petitviolet.prac.graphql
 
 import java.util.concurrent.Executors
 
-import akka.actor.{ ActorSystem, Terminated }
+import akka.actor.ActorSystem
 import akka.event.Logging
-import akka.http.scaladsl.Http
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
-import akka.http.scaladsl.model.HttpHeader
 import akka.http.scaladsl.server._
+import akka.http.scaladsl.{ Http, server }
 import akka.stream.ActorMaterializer
+import com.typesafe.config.ConfigFactory
+import org.slf4j.LoggerFactory
 import spray.json._
 
-import scala.concurrent.{ Await, ExecutionContext }
 import scala.concurrent.duration._
+import scala.concurrent.{ Await, ExecutionContext }
 import scala.io.StdIn
 
 object main extends App with Directives {
   implicit val system: ActorSystem = ActorSystem("graphql-prac")
   implicit val materializer: ActorMaterializer = ActorMaterializer()
+  private val config = ConfigFactory.load()
+  private val myLogger = LoggerFactory.getLogger(this.getClass)
+  private val awesomeLogger = LoggerFactory.getLogger("awesome_log")
 
   private val executionContext =
     ExecutionContext.fromExecutor(Executors.newFixedThreadPool(sys.runtime.availableProcessors()))
 
+  def withLogging: server.Directive0 = {
+    extractRequestContext.flatMap { ctx ⇒
+      myLogger.info(s"[request]uri: ${ctx.request.uri}, header: ${ctx.request.headers}")
+      awesomeLogger.info(s"[request]uri: ${ctx.request.uri}, header: ${ctx.request.headers}")
+      mapRouteResult { result ⇒
+        myLogger.info(s"[response]${result}")
+        awesomeLogger.info(s"[response]${result}")
+        result
+      }
+    }
+  }
+
   val route: Route =
     (get & pathPrefix("index") & extractUri & headerValueByName("User-Agent")) { (uri, ua) =>
-      logRequestResult("/index", Logging.InfoLevel) {
+      withLogging {
         complete(s"param: ${uri.query().toMap}, user-agent: ${ua}}")
       }
     } ~
+      (get & path("config") & withLogging) {
+        complete(s"env: ${config.getString("my.configuration.env")}")
+      } ~
       (post & path("graphql")) {
         entity(as[JsValue]) { jsObject =>
           logRequestResult("/graphql", Logging.InfoLevel) {
@@ -49,23 +68,21 @@ object main extends App with Directives {
 
   val f = Http().bindAndHandle(route, host, port)
 
-  println(s"server at [$host:$port]")
+  myLogger.info(s"server at [$host:$port]")
 
   Await.ready(f, Duration.Inf)
-//  val _ = StdIn.readLine("\ninput something\n")
-//
-//  println("\nshutdown...\n")
-//  val x = f.flatMap { b =>
-//    b.unbind()
-//      .flatMap { _ =>
-//        materializer.shutdown()
-//        system.terminate()
-//      }(ExecutionContext.global)
-//  }(ExecutionContext.global)
-//
-//  Await.ready(x, Duration.Inf)
-//  sys.runtime.gc()
-//  println(s"shutdown completed!\n")
+  sys.addShutdownHook {
+    myLogger.info("\nshutdown...\n")
+    val x = f.flatMap { b =>
+      b.unbind()
+        .flatMap { _ =>
+          materializer.shutdown()
+          system.terminate()
+        }(ExecutionContext.global)
+    }(ExecutionContext.global)
 
-//  sys.exit(0)
+    Await.ready(x, Duration.Inf)
+    myLogger.info(s"shutdown completed!\n")
+  }
+  val _ = StdIn.readLine("\ninput something\n")
 }
