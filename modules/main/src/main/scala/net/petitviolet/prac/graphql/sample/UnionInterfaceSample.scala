a119a462e47c368dab28a38840c7499fb4f3fd4a
petitviolet <violethero0820@gmail.com>
2018-06-11 23:13:34 +0900

    refactored

--- modules/main/src/main/scala/net/petitviolet/prac/graphql/sample/UnionInterfaceSample.scala
+++ modules/main/src/main/scala/net/petitviolet/prac/graphql/sample/UnionInterfaceSample.scala
@@ -1,128 +1,33 @@
 package net.petitviolet.prac.graphql.sample
 
-import java.util.concurrent.Executors
-
-import akka.actor.ActorSystem
-import akka.event.Logging
-import akka.http.scaladsl.Http
-import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
-import akka.http.scaladsl.model.StatusCode
-import akka.http.scaladsl.model.StatusCodes._
-import akka.http.scaladsl.server.Directives._
-import akka.http.scaladsl.server._
-import akka.stream.ActorMaterializer
-import sangria.execution._
 import sangria.macros.derive
-import sangria.marshalling.sprayJson._
-import sangria.parser.QueryParser
 import sangria.schema._
-import spray.json.{ JsObject, JsString, JsValue }
-
-import scala.concurrent._
-import scala.concurrent.duration._
-import scala.io.StdIn
-
 
 /**
  * {{{
- *
  * query MyQuery {
  *   all {
- *   __typename
- *   id
- *   name
- *   ...on Dog {
- *     kind
- *   }
- *   ...on Cat {
- *     color
+ *     __typename
+ *     id
+ *     name
+ *     ...on Dog {
+ *       kind
+ *     }
+ *     ...on Cat {
+ *       color
+ *     }
  *   }
  * }
-}
-
  * }}}
  */
-object UnionInterfaceSampleApp {
-  def main(args: Array[String]): Unit = {
+object UnionInterfaceSampleApp extends SampleApp(AnimalGraphQLServer)
 
-    implicit val system: ActorSystem = ActorSystem("graphql-prac")
-    implicit val materializer: ActorMaterializer = ActorMaterializer()
+private object AnimalGraphQLServer extends GraphQLServerBase {
+  override type Ctx = UnionInterfaceSampleSchema.AnimalRepository
 
-    val executionContext =
-      ExecutionContext.fromExecutor(Executors.newFixedThreadPool(sys.runtime.availableProcessors()))
+  override protected def schema = UnionInterfaceSampleSchema.schema
 
-    val route: Route =
-      (post & path("graphql")) {
-        entity(as[JsValue]) { jsObject =>
-//          logRequestResult("/graphql", Logging.InfoLevel) {
-          complete(AnimalGraphQLServer.execute(jsObject)(executionContext))
-//          }
-        }
-      } ~
-        get {
-          logRequestResult("/graphiql.html", Logging.InfoLevel) {
-            getFromResource("graphiql.html")
-          }
-        }
-
-    val host = sys.props.get("http.host") getOrElse "0.0.0.0"
-    val port = sys.props.get("http.port").fold(8080)(_.toInt)
-
-    val f = Http().bindAndHandle(route, host, port)
-
-    println(s"server at [$host:$port]")
-
-    val _ = StdIn.readLine("\ninput something\n")
-
-    println("\nshutdown...\n")
-    val x = f.flatMap { b =>
-      b.unbind()
-        .flatMap { _ =>
-          materializer.shutdown()
-          system.terminate()
-        }(ExecutionContext.global)
-    }(ExecutionContext.global)
-
-    Await.ready(x, 5.seconds)
-    sys.runtime.gc()
-    println(s"shutdown completed!\n")
-  }
-}
-
-private object AnimalGraphQLServer {
-  private val repository = new UnionInterfaceSampleSchema.AnimalRepository()
-
-  def execute(jsValue: JsValue)(implicit ec: ExecutionContext): Future[(StatusCode, JsValue)] = {
-    val JsObject(fields) = jsValue
-    val operation = fields.get("operationName") collect {
-      case JsString(op) => op
-    }
-
-    val vars = fields.get("variables") match {
-      case Some(obj: JsObject) => obj
-      case _                   => JsObject.empty
-    }
-
-    val Some(JsString(document)) = fields.get("query") orElse fields.get("mutation")
-
-    Future.fromTry(QueryParser.parse(document)) flatMap { queryDocument =>
-      Executor
-        .execute(
-          UnionInterfaceSampleSchema.schema,
-          queryDocument,
-          repository,
-          operationName = operation,
-          variables = vars
-        )
-        .map { jsValue =>
-          OK -> jsValue
-        }
-        .recover {
-          case error: QueryAnalysisError => BadRequest -> error.resolveError
-          case error: ErrorWithResolver  => InternalServerError -> error.resolveError
-        }
-    }
-  }
+  override protected def context = new UnionInterfaceSampleSchema.AnimalRepository()
 }
 
 private object UnionInterfaceSampleSchema {
